25b28d579842be563f97c20456a2b8e00d008b15
Isaque Barbosa da Silva <isaque_barbosa@jabil.com>
2024-01-05 07:33:09 -0300

    primeiro commit

---
+++ MinutosTelefone/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinutosTelefone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Insira abaixo o valor em minutos que o cliente utilizou:");
+
+            int min;
+            min = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Minutos utilizados: " + min);
+
+            if(min <= 100) {
+
+                Console.WriteLine("Valor a pagar: R$ 50,00");
+
+            } else {
+
+                int valor2;
+                valor2 = ((min - 100) * 2) + 50;
+
+                Console.WriteLine("Valor a pagar: R$ " + valor2 + ",00");
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
